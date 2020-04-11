90175c3065ed934ef60a7824c9b08047d08a156d
AndresGuerrero <softmild2106@gmail.com>
2020-04-11 01:00:47 -0500

    init front

--- ClientApp/book-manager/src/app/app-routing.module.ts
+++ ClientApp/book-manager/src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BookComponent } from './views/book/book.component';
+import { CategoryComponent } from './views/category/category.component';
+import { AuthorComponent } from './views/author/author.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: BookComponent },
+  { path: 'book', component: BookComponent },
+  { path: 'category', component: CategoryComponent },
+  { path: 'author', component: AuthorComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- ClientApp/book-manager/src/app/app.module.ts
+++ ClientApp/book-manager/src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { CategoryComponent } from './views/category/category.component';
+import { AuthorComponent } from './views/author/author.component';
+import { BookComponent } from './views/book/book.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MaterialModule } from './material-module';
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoryComponent,
+    AuthorComponent,
+    BookComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
